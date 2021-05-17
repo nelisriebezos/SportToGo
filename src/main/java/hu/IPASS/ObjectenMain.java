aa272cc9c50f9d48bbb16859ebecb36b557c3664
nelisriebezos <niels@riezebos.net>
2021-05-17 22:29:40 +0200

    schema ding

--- src/main/java/hu/IPASS/ObjectenMain.java
+++ src/main/java/hu/IPASS/ObjectenMain.java
@@ -17,6 +17,9 @@ public class ObjectenMain
         geb1.addSchema(new Schema("testSchema1"));
         geb1.addSchema(new Schema("testSchema2"));
 
+        geb1.getSchema("testSchema1").addOefening(new Oefening(40, 10, squats));
+        geb1.getSchema("testSchema1").addOefening(new Oefening((10, 20, crunches));
 
+        geb1.getSchema("testSchema1").getOefening(Oefening(40,10, squats));
     }
 }
